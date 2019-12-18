a1236adfc265bd93f9e0f3f7b991d71af959d602
vsgirelli <vavi_sg@hotmail.com>
2019-12-18 01:38:26 -0300

    Trying to fix things

--- client.py
+++ client.py
@@ -10,13 +10,13 @@ if len(sys.argv) is not 3:
 host = sys.argv[1]  # The server's hostname or IP address
                     # Testing in the lab, change for the other computer IP
 port = int(sys.argv[2]) # The port used by the server
-data = ['v']*64
+data = ['v']*64000
 data = ' '.join(data).encode()
-print(data)
-print(sys.getsizeof(data))
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, port))
 
 while True:
-    s.sendall(data)
+    s.send(data)
+
+s.close()
--- server.py
+++ server.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 import socket
-from _thread import *
-import threading 
+import thread
+import threading
 import sys
 import csv
-
+from time import sleep, time
 
 if len(sys.argv) is not 2:
     print("Wrong number of arguments: python3 server.py <server_port>")
@@ -18,37 +18,41 @@ port = int(sys.argv[1]) # Port to listen on (non-privileged ports are > 1023)
 data = ['v']*64000
 size = sys.getsizeof(' '.join(data))
 
+val_read = 0
 
-def connection(c):
+def log_thread():
     filename = 'log_' + str(port) + '.csv'
     with open(filename,'w') as csv_file:
         writer = csv.writer(csv_file, delimiter=',')
+        old_val = 0
         while True:
-            data = c.recv(size).decode()
-            if not data:
-                break # probably remove this too and add the functions to the log
-            print(data)
+            val = val_read
+            row = [0,0]
+            row[0] = int(time())
+            row[1] = val - old_val
+            old_val = val
+            writer.writerow(row)
+            sleep(1)
 
-    # when the socket does not recveive any data, the connection was closed by the cient
-    c.close()
 
+# AF_INET is IPv4, and a SOCK_STREAM specify that the socket is TCP
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((host, port)) # used to associate the socket with a specific interface and port number
 
-def main():
-    # AF_INET is IPv4, and a SOCK_STREAM specify that the socket is TCP
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((host, port)) # used to associate the socket with a specific interface and port number
-    print('Listening...')
-    s.listen()
+print('Listening...')
+s.listen(1)
+
+thread.start_new_thread(log_thread, ())
+
+while True:
+    conn, addr = s.accept() # returns a new socket object
+    # conn is a socket object different from the s socket above
+    # for each accepted connection, a new socket is created
+    print('Connected by', addr[1])
+
     while True:
-        conn, addr = s.accept() # returns a new socket object
-        # conn is a socket object different from the s socket above
-        # for each accepted connection, a new socket is created
-        print('Connected by', addr)
+        rcv = conn.recv(size)
+        if not rcv:
+            break
+        val_read += sys.getsizeof(rcv)
 
-        # function to call and list of arguments
-        start_new_thread(connection, (conn,))
-
-    s.close()
-
-
-main()
