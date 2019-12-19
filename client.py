bd474620edd049de7e22fd3ec7e001ba9e3f2b91
vsgirelli <vsgirelli@inf.ufrgs.br>
2019-12-19 15:26:35 -0300

    New client

--- client.py
+++ client.py
@@ -11,12 +11,17 @@ host = sys.argv[1]  # The server's hostname or IP address
                     # Testing in the lab, change for the other computer IP
 port = int(sys.argv[2]) # The port used by the server
 data = ['v']*64000
-data = ' '.join(data).encode()
+#size = data.size()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, port))
 
 while True:
-    s.send(data)
+    #t2 = time.time()
+    s.send(','.join(data))
+    #t3 = time.time()
+
+    #throughput = size / (t3 - t2)
+    #print(throughput)
 
 s.close()
